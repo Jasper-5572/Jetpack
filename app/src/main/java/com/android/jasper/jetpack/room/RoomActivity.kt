8d464e3d9b8b2ebb78413f799705f5a5c798ee90
hujc <13365755735@163.com>
2020-04-10 11:08:00 +0800

    Initial commit

---
+++ app/src/main/java/com/android/jasper/jetpack/room/RoomActivity.kt
@@ -0,0 +1,15 @@
+package com.android.jasper.jetpack.room
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *@author   Jasper
+ *@create   2020/4/10 10:50
+ *@describe
+ *@update
+ */
+class RoomActivity: AppCompatActivity() {
+
+
+}
